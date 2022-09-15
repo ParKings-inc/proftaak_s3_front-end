eaf3d4f01bb4357963f87deac51b948bda378e15
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2022-09-15 12:24:38 +0200

    Add login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
@@ -22,7 +23,7 @@ function App() {
       </div>
       <Routes>
         <Route path="/" element={<HomePage></HomePage>}></Route>
-        <Route path="/login" element={<div></div>}></Route>
+        <Route path="/login" element={<LoginPage></LoginPage>}></Route>
         <Route path="/signup" element={<div></div>}></Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React, { Component, ReactNode } from "react";
+
+export default class LoginPage extends Component {
+    public override render(): ReactNode {
+        return (
+            <div>Login Page</div>
+        );
+    }
+}
