2191e0fbbe56f6e60eed9bd55dc24cff47ae8b1b
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2022-09-22 10:32:15 +0200

    Add Google login button

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,19 @@
-import React, { Component, ReactNode } from "react";
+import { Component, ReactNode } from "react";
+import './LoginPage.css';
+import GoogleLogo from '../assets/logos/google.svg';
 
 export default class LoginPage extends Component {
     public override render(): ReactNode {
         return (
-            <div>Login Page</div>
+            <div className="centre">
+                <div className="title">Log In</div>
+                <div className="login-container">
+                    <button className="login-button">
+                        <img src={GoogleLogo} alt="Google Logo"></img>
+                        <div className="login-text">Log in with Google</div>
+                    </button>
+                </div>
+            </div>
         );
     }
 }
---
+++ src/tests/pages/LoginPage.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import LoginPage from '../../pages/LoginPage';
+
+
+test("Login page renders title", () => {
+  render(<LoginPage />);
+  const titleElement = screen.getByText(/^Log In$/i);
+  expect(titleElement).toBeInTheDocument();
+});
