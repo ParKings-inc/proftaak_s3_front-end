a919dfe9bf083b5f864ac70ab09500ee94fbb4b6
davey2206 <davey2206@live.nl>
2022-09-22 11:32:00 +0200

    removed proxy

--- src/services/CarService.js
+++ src/services/CarService.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 export async function getAllCars()
 {
     try{
-        const response = await axios.get('/api/Autos');
+        const response = await axios.get('https://localhost:7205/api/Autos');
         console.log('response ', response)
         return response.data;
     }
--- src/services/ParkingService.js
+++ src/services/ParkingService.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 export async function getAllParking()
 {
     try{
-        const response = await axios.get('/api/Parkings');
+        const response = await axios.get('https://localhost:7205/api/Parkings');
         console.log('response ', response)
         return response.data;
     }
@@ -15,7 +15,7 @@ export async function getAllParking()
 export async function getAllParkingPerCar(data)
 {
     try{
-        const response = await axios.get('/api/Parkings', {car: data});
+        const response = await axios.get('https://localhost:7205/api/Parkings', {car: data});
         console.log('response ', response)
         return response.data;
     }
