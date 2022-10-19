1e510409354f90f45ea84e8f014a3b5003ea7fae
Jordy Walraven <jordywalraven1@gmail.com>
2022-10-19 16:34:16 +0200

    Adding more logic to display

--- src/components/FreeSpaceDisplay.tsx
+++ src/components/FreeSpaceDisplay.tsx
@@ -3,16 +3,16 @@ import { getFreeSpaces } from "../services/ParkingService";
 import "../style/shapes.css";
 
 interface State {
-    freeSpaces: number;
-    totalSpaces: number;
+  freeSpaces: number;
+  totalSpaces: number;
 }
 
 export default class FreeSpaceDisplay extends Component<{}, State> {
   constructor(props: any) {
     super(props);
     this.state = {
-        freeSpaces: 0,
-        totalSpaces: 0,
+      freeSpaces: 0,
+      totalSpaces: 0,
     }
   }
 
@@ -22,32 +22,32 @@ export default class FreeSpaceDisplay extends Component<{}, State> {
     console.log(Spaces)
     let freespaces = Spaces.data[1] - Spaces.data[0];
     let totalSpaces = Spaces.data[1];
-    this.setState({freeSpaces: freespaces});
-    this.setState({totalSpaces: totalSpaces})
-    this.setColor(freespaces,Spaces.data[1]);
+    this.setState({ freeSpaces: freespaces });
+    this.setState({ totalSpaces: totalSpaces })
+    this.setColor(freespaces, Spaces.data[1]);
   }
 
-  setColor(freeSpaces: number,totalSpaces:number){
-    let circle =document.getElementById("colorCircle")
-    let percentageValue = freeSpaces/totalSpaces*100;
+  setColor(freeSpaces: number, totalSpaces: number) {
+    let circle = document.getElementById("colorCircle")
+    let percentageValue = freeSpaces / totalSpaces * 100;
     console.log(percentageValue)
-    let color = `hsla(${10+percentageValue},100%,50%)`;
-    if(circle)
-    circle.style.backgroundColor = color;
+    let color = `hsla(${10 + percentageValue},100%,50%)`;
+    if (circle)
+      circle.style.backgroundColor = color;
   }
 
   componentDidMount(): void {
     this.assignFreeSpaces(1);
-    
+
   }
-//#endregion
-  
-render() {
+  //#endregion
+
+  render() {
     return (
       <>
         <div className="d-flex flex-row justify-content-center">
           <div id="colorCircle" className="circle align-self-center me-1"></div>{" "}
-            <h2 className="m-0">Free Spaces: {this.state.freeSpaces} <small className="text-muted">(Out of {this.state.totalSpaces})</small></h2>
+          <h2 className="m-0">Free Spaces: {this.state.freeSpaces} <small className="text-muted">(Out of {this.state.totalSpaces})</small></h2>
         </div>
       </>
     );
