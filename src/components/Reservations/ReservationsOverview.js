819c38aeaaf9fa85960e627021789e89fbe0c1c1
Gökay <tuhezhen@hotmail.com>
2022-10-29 10:46:49 +0200

    Changing UI Reservations
    Added header + non-working back button. Going to change style of cards + add two new buttons.

--- src/components/Reservations/ReservationsOverview.js
+++ src/components/Reservations/ReservationsOverview.js
@@ -1,36 +1,34 @@
 import React, { useEffect } from 'react'
 import { getAllReservations } from "../../services/ReservationService";
+import "../../style/ReservationsPage.css";
 
 const ReservationOverviewPage = (props) => {
 
     let empty = []
     console.log(props)
     return (
-        <div>
-            <ul>
-                <hr />
-                {props.reservations.map(reservatie => {
-                    return (
-                        <div>
-                            <h3>Reservation:  {' ' + reservatie.id}</h3>
-                            <br />
-                            <li>
-                                <b> Reservation spaceID:</b>  {' ' + reservatie.spaceID}
-                            </li>
-                            <li>
-                                <b> Reservation carID:</b>  {' ' + reservatie.carID}
-                            </li>
-                            <li>
-                                <b>  Reservation arrivalTime:</b>  {' ' + reservatie.arrivalTime}
-                            </li>
-                            <li>
-                                <b> Reservation departureTime:</b> {' ' + reservatie.departureTime}
-                            </li>
-                            <hr />
-                        </div>
-                    )
-                })}
-            </ul>
+        <div className='w-65'>
+            {props.reservations.map(reservatie => {
+                return (
+                    <div>
+                        <h3>Reservation:  {' ' + reservatie.id}</h3>
+                        <br />
+                        <li>
+                            <b> Reservation spaceID:</b>  {' ' + reservatie.spaceID}
+                        </li>
+                        <li>
+                            <b> Reservation carID:</b>  {' ' + reservatie.carID}
+                        </li>
+                        <li>
+                            <b>  Reservation arrivalTime:</b>  {' ' + reservatie.arrivalTime}
+                        </li>
+                        <li>
+                            <b> Reservation departureTime:</b> {' ' + reservatie.departureTime}
+                        </li>
+                        <hr />
+                    </div>
+                )
+            })}
         </div>
     )
 }
--- src/pages/ReservationPages/ReservationOverviewPage.js
+++ src/pages/ReservationPages/ReservationOverviewPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { getAllReservations } from "../../services/ReservationService";
 import ReservationsOverview from "../../components/Reservations/ReservationsOverview";
+import "../../style/ReservationsPage.css";
+import { Typography } from '@mui/material';
 
 const ReservationOverviewPage = () => {
 
@@ -14,7 +16,18 @@ const ReservationOverviewPage = () => {
 
 
     return (
-        <div>
+        <div className='center mt-25'>
+            <div className='flex row mb-25'>
+                {/* Change Icon */}
+                <div className='w-auto bg-primary px-3 py-2 rounded'>
+                    <h5 className='m-0'>X</h5>
+                </div>
+                {/* Align text vertically */}
+                <Typography className='w-auto' variant='h5'>My Reservations</Typography>
+            </div>
+            <div className='flex row'>
+                
+            </div>
             <ReservationsOverview reservations={reservations}></ReservationsOverview>
         </div>
     )
