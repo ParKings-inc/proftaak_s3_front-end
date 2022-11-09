2bd1df4b3be5ee9b0b66dd0825795006cd4da237
Jordy Walraven <jordywalraven1@gmail.com>
2022-11-09 14:09:10 +0100

    Adding routes + toast

--- src/App.js
+++ src/App.js
@@ -19,15 +19,16 @@ import ReservationAvailableSpaces from "./pages/ReservationPages/Create/Reservat
 import ReservationDetailsPage from "./pages/ReservationPages/View/Details/ReservationDetailsPage";
 import ReservationUpdatePage from "./pages/ReservationPages/Update/ReservationUpdatePage";
 import ReservationDeletePage from "./pages/ReservationPages/Delete/ReservationDeletePage";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const [stateUser, setStateUser] = useState(null);
-    const service = new AccountService();
-    const value = {
-        user: stateUser,
-        userLogin: loginUser,
-        userLogout: logoutUser,
-    };
+  const service = new AccountService();
+  const value = {
+    user: stateUser,
+    userLogin: loginUser,
+    userLogout: logoutUser,
+  };
 
   function loginUser(stateCredentials) {
     console.log("PASSED IN USER");
@@ -55,28 +56,37 @@ function App() {
   return (
 
     <>
-    <userContext.Provider value={value}>
-      <BrowserRouter>
-        <NavigationBar value={stateUser} logout={logoutUser}></NavigationBar>
+    <ToastContainer></ToastContainer>
+      <userContext.Provider value={value}>
+        <BrowserRouter>
+          <NavigationBar value={stateUser} logout={logoutUser}></NavigationBar>
           <Routes>
-                <Route path="/" element={<HomePage></HomePage>}></Route>
-                {stateUser != null ? (
-                <Route
-                    path="/carpage"
-                    element={<AddCarPage></AddCarPage>}
-                ></Route>
-                ) : (
-                <></>
-                )}
-                <Route
-                path="/login"
-                element={<LoginPage value={value} ></LoginPage>}
-                ></Route>
-                <Route path="/signup" element={<SignUpPage></SignUpPage>}></Route>
-                <Route path="/reservations" element={<ReservationOverviewPage></ReservationOverviewPage>}></Route>
-        </Routes>
-      </BrowserRouter>
-    </userContext.Provider>
+            <Route path="/" element={<HomePage></HomePage>}></Route>
+            {stateUser != null ? (
+              <Route
+                path="/carpage"
+                element={<AddCarPage></AddCarPage>}
+              ></Route>
+            ) : (
+              <></>
+            )}
+            <Route
+              path="/login"
+              element={<LoginPage value={value} ></LoginPage>}
+            ></Route>
+            <Route path="/signup" element={<SignUpPage></SignUpPage>}></Route>
+            <Route path="/reservations" element={<ReservationOverviewPage></ReservationOverviewPage>}></Route>
+            <Route path="/reservations/create" element={<ReservationsCreatePage></ReservationsCreatePage>}></Route>
+            <Route path="/reservations/availableSpaces/:LicensePlate/:ArrivalTime/:DepartureTime/:GarageId" element={<ReservationAvailableSpaces />} />
+            {/* Details */}
+            <Route path="/reservation/Details" element={<ReservationDetailsPage />}></Route>
+            {/* Update */}
+            <Route path="/reservation/Update" element={<ReservationUpdatePage />}></Route>
+            {/* Delete */}
+            <Route path="/reservation/Delete" element={<ReservationDeletePage />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </userContext.Provider>
     </>
 
   );
