9f9a8260c2ffca91300d4cca95cabded257e323d
Gökay <tuhezhen@hotmail.com>
2022-11-15 23:21:19 +0100

    Minor css fix

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -59,7 +59,7 @@ export default function HomePage() {
     return (
         <div className="eighty-screen flex flex-column">
             <div className="full-width full-height" style={{display: "Flex", flexDirection: "column"}}>
-                <div className="bg-primary h-40 skew flex horizontal-center padding-top-100">
+                <div className="bg-primary h-40 skew flex horizontal-center padding-top-50">
                     <div className="flex column vertical-center">
                         { stateUser != null && <Avatar alt={stateUser.name} src={stateUser.picture} sx={{ width: 100, height: 100, marginBottom: "20px" }}/> }
                         { stateUser != null && <Typography className="text-white" variant="h5">{ stateUser.name }</Typography> }
@@ -68,7 +68,7 @@ export default function HomePage() {
                 <div className="flex column vertical-center h-50">
                     <Typography className="text-black" variant="h6">You have <span className="text-primary text-26"> { stateReservations.length } </span> reservations today!</Typography>
 
-                    <Carousel className="bg-primary" style={{minHeight: "30%", width: "80%", backgroundColor: "#b8b8b8", marginBottom: "20px", padding: "10px"}}>
+                    <Carousel className="bg-primary" style={{width: "80%", backgroundColor: "#b8b8b8", marginBottom: "20px", padding: "10px"}}>
                             { stateReservations.map((reservation, index) => {
                                 const arrivalDate = new Date(reservation.ArrivalTime);
                                 const arrivalTime = `${arrivalDate.getHours()}:${String(arrivalDate.getMinutes()).padStart(2, '0')}`;
@@ -77,16 +77,10 @@ export default function HomePage() {
                                 const departureTime = `${departureDate.getHours()}:${String(departureDate.getMinutes()).padStart(2, '0')}`;
 
                                 return (
-                                    <CarouselItem>
-                                        <div className='w-80 h-40' style={{margin: "auto"}} key={index}>
+                                    <CarouselItem key={index}>
+                                        <div className='w-80 h-40' style={{margin: "auto", overflow: "auto"}}>
                                             <Card className=''>
                                                 <CardContent className='p-3'>
-                                                    <div className='flex flex-row justify-between'>
-                                                        <Typography variant="h5" component="div">
-                                                            {reservation.GarageName}
-                                                        </Typography>
-
-                                                    </div>
                                                     <div className='flex flex-column justify-between'>
                                                         <Typography color="text.secondary">
                                                             <b>Date:</b> {date}
@@ -120,7 +114,7 @@ export default function HomePage() {
                             }
                         </Carousel>
 
-                        <div className="flex row full-width space-evenly full-height px-4">
+                        <div className="flex row full-width space-evenly full-height max-h-35 px-4">
                             <div className="w-40 full-height border rounded-3 shadow flex column vertical-center horizontal-center">
                                 <LocalParkingIcon className="text-primary" sx={{fontSize: "60px", marginBottom: "10px"}}/>
                                 <Typography variant="body1">Enter Garage</Typography>
@@ -131,93 +125,7 @@ export default function HomePage() {
                             </div>
                         </div>
                 </div>
-
-
-
-
-
-                    {/* <Typography className="text-white" variant="h5">Logged in as:</Typography>
-                    <div className="bg-blue-600" style={{width: "75%", height: "15%", borderTopLeftRadius: "50px", borderBottomLeftRadius: "50px", borderTopRightRadius: "50px", borderBottomRightRadius: "50px"}}>
-                        <div className="flex flex-row full-height">
-                            <div style={{paddingLeft: "5%", marginRight: "5%", display: "flex", alignItems: "center"}}>
-                                <Avatar alt={user.name} src={user.picture}/>
-                            </div>
-                            <div className="flex flex-column full-height vertical-center">
-                                <Typography className="text-white" variant="h6">{ user.name }</Typography>
-                                <Typography className="text-muted">{ user.email }</Typography>
-                            </div>
-                            
-                        </div>
-                    </div>
-
-                    <div style={{width: "75%", marginTop: "5%", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
-                        <Typography className="text-white margin-bottom-30" variant="h6">You have <span className="">{ stateReservations.length }</span> reservations today</Typography>
-
-                        <Carousel className="h-65 full-width">
-                            { stateReservations.map((reservation, index) => {
-                                const arrivalDate = new Date(reservation.ArrivalTime);
-                                const arrivalTime = `${arrivalDate.getHours()}:${String(arrivalDate.getMinutes()).padStart(2, '0')}`;
-                                const date = dayjs(reservation.ArrivalTime).format("DD-MM-YYYY")
-                                const departureDate = new Date(reservation.DepartureTime);
-                                const departureTime = `${departureDate.getHours()}:${String(departureDate.getMinutes()).padStart(2, '0')}`;
-
-                                return (
-                                    <CarouselItem>
-                                    <div className='w-100 mb-3 bg-black' key={index}>
-                                        <Card className='left-border border-primary'>
-                                            <CardContent className='p-3'>
-                                                <div className='flex flex-row justify-between'>
-                                                    <Typography variant="h5" component="div">
-                                                        {reservation.GarageName}
-                                                    </Typography>
-
-                                                </div>
-                                                <div className='flex flex-row justify-between'>
-                                                    <Typography color="text.secondary">
-                                                        <b>Date:</b> {date}
-                                                    </Typography>
-                                                    <Typography color="text.secondary">
-                                                        <b>Time:</b> {arrivalTime + ' - ' + departureTime}
-                                                    </Typography>
-                                                </div>
-
-                                                <div className='flex flex-row justify-between'>
-                                                    <Typography color="text.secondary">
-                                                        <b>Space:</b> {reservation.SpaceID}-{reservation.SpaceFloor}-{reservation.SpaceRow}
-                                                    </Typography>
-
-                                                </div>
-                                                <div className='flex flex-row justify-between'>
-                                                    <Typography color="text.secondary">
-                                                        <b>License plate:</b> {reservation.Kenteken}
-                                                    </Typography>
-                                                </div>
-                                                <Typography className='text-warning' sx={{ marginTop: 1 }} color="text.primary">
-                                                    {reservation.Status}
-                                                </Typography>
-                                            </CardContent>
-                                        </Card>
-                                    </div>
-                                </CarouselItem>
-                                )
-
-                            }) 
-                            }
-                        </Carousel>
-                    </div> */}
-
-            </div>
-            {/* <div className="full-width h-65 border-radius-40 p-3 skew" style={{backgroundColor: "#f4f4f4", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-evenly"}}>
-
-                <Button variant="contained" startIcon={<LocalParkingIcon />}>
-                    Enter Garage
-                </Button>
-                
-                <Button variant="contained" startIcon={<SettingsIcon />}>
-                    Manage
-                </Button>
-            </div> */}
-                            
+            </div>  
         </div>
     );
 }
