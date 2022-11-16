3fd05c459c6c0cfd19969efd1a4669988d044658
LisaKarto <101811602+LisaKarto@users.noreply.github.com>
2022-11-16 23:03:50 +0100

    Check to see if reservation time is currently active upon request to update or delete reservation implemented

--- src/components/Reservations/Details/ReservationDetails.js
+++ src/components/Reservations/Details/ReservationDetails.js
@@ -7,20 +7,62 @@ import {
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import dayjs from "dayjs";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ReservationDetails = () => {
     const location = useLocation();
     let reservation = location.state.reservation[0];
     const navigate = useNavigate();
 
+    const date = //"2022-11-17T13:05:00"; // <-- test date
+        dayjs(new Date()).format("YYYY-MM-DDTHH:mm:ss");
+
+    // logic should be changed once our scanner simulation is complete. Perhaps you should be able to cancel or update a reservation based on wheter you are in the garage or not.
+    // instead of checking based of the time of a reservation.
+
     function UpdateReservation() {
-        navigate("/reservation/Update", { state: { reservation: [reservation] } });
+        if (date < reservation.ArrivalTime) {
+            navigate("/reservation/Update", { state: { reservation: [reservation] } });
+        } else if (date > reservation.ArrivalTime) {
+            toasrMessage("error", "Cannot update a active reservation.");
+        }
     }
 
     function CancelReservation() {
-        navigate("/reservation/Delete", { state: { reservation: [reservation] } });
+        if (date < reservation.ArrivalTime) {
+            navigate("/reservation/Delete", { state: { reservation: [reservation] } });
+        } else if (date > reservation.ArrivalTime) {
+            toasrMessage("error", "Cannot cancel a active reservation.");
+        }
     }
 
+    // toastr
+    function toasrMessage(type, message) {
+        if (type == "error") {
+            toast.error(message, {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored"
+            })
+        } else {
+            toast.success(message, {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored"
+            })
+        }
+    }
 
     return (
         <div>
