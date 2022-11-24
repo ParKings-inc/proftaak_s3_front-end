bfdcdb1c011d221e90b3a801fd6397c3d8366418
JaimyDerks <78917274+JaimyDerks@users.noreply.github.com>
2022-11-24 11:05:15 +0100

    Added Licence-Check

---
+++ src/components/LicenseValidation/kenteken-check-nl.js
@@ -0,0 +1,111 @@
+const el = document.getElementById('kenteken');
+const inputElm = document.getElementById('input-kenteken');
+
+// start function kentekenCheck
+const kentekenCheck = ((elm) => {
+
+    let classValid = '';
+    let classInvalid = '';
+    let invalidString = '';
+    let newStr = '';
+    let index = 0;
+    const arrRegEx = ['^([A-Z]{2})([0-9]{2})([0-9]{2})$', // XX9999
+        '^([0-9]{2})([0-9]{2})([A-Z]{2})$', // 9999XX
+        '^([0-9]{2})([A-Z]{2})([0-9]{2})$', // 99XX99
+        '^([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{2})$',// XX99XX
+        '^([BDFGHJKLMNPRSTVWXYZ]{2})([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})$',// XXXX99
+        '^([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{2})([BDFGHJKLMNPRSTVWXYZ]{2})$',// 99XXXX
+        '^([0-9]{2})([BDFGHJKLMNPRSTVWXYZ]{3})([0-9]{1})$',// 99XXX9
+        '^([0-9]{1})([BDFGHJKLMNPRSTVWXYZ]{3})([0-9]{2})$',// 9XXX99
+        '^([BDFGHJKLMNPRSTVWXYZ]{2})([0-9]{3})([BDFGHJKLMNPRSTVWXYZ]{1})$',// XX999X
+        '^([BDFGHJKLMNPRSTVWXYZ]{1})([0-9]{3})([BDFGHJKLMNPRSTVWXYZ]{2})$',// X999XX
+        '^([BDFGHJKMNPRSVWXYZ]{3})([0-9]{2})([BDFGHJKMNPRSVWXYZ]{1})$',// XXX99X 11
+        '^([0-9]{1})([BDFGHJKMNPRSVWXYZ]{2})([0-9]{3})$',//9XX999 13
+        '^([0-9]{3})([BDFGHJKMNPRSVWXYZ]{2})([0-9]{1})$'//999XX9 14
+    ];
+
+    const forbiddenCharacters = /^((?!GVD|KKK|KVT|LPF|NSB|PKK|PSV|TBS|SS|SD|PVV|SGP|VVD).){8}$/;
+
+    // based on rdw demands
+    // returns true immediately when found match : legacy browser proof IE 9/10/11, no polyfill needed
+    const matchLicense = str => {
+        return arrRegEx.some((regEx, i) => {
+
+            const re = new RegExp(regEx);
+            const result = re.test(str);
+
+
+            // match on regex pattern
+            if (result) {
+                index = i;
+                return true;
+            }
+        });
+    };
+
+    const checkForbiddenCharacters = (str) => {
+        return forbiddenCharacters.test(str);
+    }
+
+
+    const showLicense = (str) => {
+        const match = matchLicense(str);
+
+        if (match) {
+            const re = new RegExp(arrRegEx[index]);
+
+            elm.value = str.replace(re, '$1-$2-$3');
+            elm.classList.remove(classInvalid);
+            elm.classList.add(classValid);
+
+            newStr = str.replace(re, '$1-$2-$3');
+            const notForbidden = checkForbiddenCharacters(newStr)
+            if (notForbidden) {
+                return newStr;
+            }
+        }
+
+        elm.classList.remove(classValid);
+        elm.classList.add(classInvalid);
+
+        return invalidString; // XX-XX-XX
+    };
+
+    const getLicense = (license, classV = 'valid', classInv = 'invalid', invalidStr = 'XX-XX-XX') => {
+        if (typeof license !== 'string') return;
+
+        classValid = classV;
+        classInvalid = classInv;
+        invalidString = invalidStr;
+        const str = license.toUpperCase()
+            .trim()
+            .split('-')
+            .join(''); // trim whitespace / strip dashes
+        return showLicense(str);
+    };
+
+    return {
+        getLicense,
+    }
+
+
+})(inputElm);
+
+// vervang het voorbeeld met een geldig kenteken zonder/met verkeerd geplaatste koppeltekens
+// bijvoorbeeld 12TTHJ HFFF43 of 1KGF55 of G234TR H222GG, HF-FF43 , G-234-TR
+
+
+// om met performance rekening te houden kan wellicht ook het change event worden gebruikt
+
+//default example
+
+el.innerHTML = kentekenCheck.getLicense('S007JB');
+
+
+inputElm.addEventListener('input', (e) => {
+    // returns string
+    if (e.target.value.length >= 6){
+        const license =  kentekenCheck.getLicense(e.target.value);
+        el.innerHTML = license;
+    }
+});
--- src/pages/AddCarPage.js
+++ src/pages/AddCarPage.js
@@ -15,6 +15,8 @@ import { getAllCars } from '../services/CarService'
 import { getCarByUserId } from '../services/CarService'
 import { DeleteCar } from '../services/CarService'
 import '../style/AddCarPage.css'
+import {KentekenCheck} from '../components/LicenseValidation/kenteken-check-nl-class.js'
+//import {KentekenCheck} from 'rdw-kenteken-check'
 
 
 
@@ -31,15 +33,33 @@ const AddCarPage = () => {
       AssignCars()
     }, [])
 
+
+    function licenseValidation(){
+      const outputElm = document.getElementById('kenteken-inline');
+      const inputElm = document.getElementById('license');
+
+      const kt = new KentekenCheck(document.getElementById('license').value, inputElm, outputElm, true);
+      console.log("License validation")
+      //kt.formatLicense();
+      kt.formatLicense();
+      kt.bindInputListener();
+      console.log("Kentekencheck output: ", kt.valid)
+
+      // format only
+      const kt2 = new KentekenCheck('JFK01P')
+      outputElm.innerHTML = kt2.formatLicense();
+    }
+    
     function OnSubmit(){
-        let LicensePlate = document.getElementById('license').value
-        let data = {
+        let LicensePlate = document.getElementById('license').value;
+        licenseValidation();
+        /*let data = {
             userID: user.sub,
             kenteken: LicensePlate
           }
         console.log(data)
         console.log(user)
-        AddCar(data)
+        AddCar(data)*/
     }
 
 
@@ -57,6 +77,18 @@ const AddCarPage = () => {
       <b className='list-item'>{c.kenteken}</b>
       <button className='delete-button' onClick={() => {DeleteCar(c.id)}}>delete</button>
       </p>)}</div>
+      
+<div class="form-control">
+    <label for="kenteken-inline">html5 validation</label>
+    <input id="kenteken-inline"
+           class="html5-input"
+           type="text"
+           maxlength="6"
+           autocomplete="off"
+           pattern="^([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})([0-9]{2})|([0-9]{2})([0-9]{2})([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{2})|([0-9]{2})([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})|([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{2})|([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{2})([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{2})([0-9]{2})|([0-9]{2})([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{2})([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{2})|([0-9]{2})([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{3})([0-9]{1})|([0-9]{1})([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{3})([0-9]{2})|([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{2})([0-9]{3})([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{1})|([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{1})([0-9]{3})([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{2})|([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{3})([0-9]{2})([bdfghjklmnprstvwxyzBDFGHJKLMNPRSTVWXYZ]{1})$"
+           required/>
+    <span class="valid-message"></span>
+</div>
     </div>
     
   )
